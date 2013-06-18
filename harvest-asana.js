00aa1376ca5e10b4c76c1d60e929d88d62e01795
David Robinson <dgrobinson@gmail.com>
2013-06-18 17:51:16 -0400

    initial commit

---
+++ harvest-asana.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name       Asana Harvest Integration
+// @namespace  http://use.i.E.your.homepage/
+// @version    0.1
+// @description  enter something useful
+// @match      http://*/*
+// @copyright  2012+, You
+// ==/UserScript==
+
